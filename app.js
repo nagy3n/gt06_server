8e5681d41077308565b741410191339a024ae1a3
Nagy Daoud <nagy.daoud@deliveryhero.com>
2022-08-21 18:50:48 +0200

    lol

--- app.js
+++ app.js
@@ -5,29 +5,29 @@ const net = require('net');
 const fs = require('fs');
 
 const serverPort = process.env.GT06_SERVER_PORT || 64459;
-const rootTopic = process.env.MQTT_ROOT_TOPIC || 'gt06';
-const brokerUrl = process.env.MQTT_BROKER_URL || 'localhost';
-const brokerPort = process.env.MQTT_BROKER_PORT || 1883;
-const mqttProtocol = process.env.MQTT_BROKER_PROTO || 'mqtt';
-const brokerUser = process.env.MQTT_BROKER_USER || 'user';
-const brokerPasswd = process.env.MQTT_BROKER_PASSWD || 'passwd';
-const trustedCaPath = process.env.MQTT_BROKER_CA || '';
-const TRUSTED_CA = fs.readFileSync(trustedCaPath);
+// const rootTopic = process.env.MQTT_ROOT_TOPIC || 'gt06';
+// const brokerUrl = process.env.MQTT_BROKER_URL || 'localhost';
+// const brokerPort = process.env.MQTT_BROKER_PORT || 1883;
+// const mqttProtocol = process.env.MQTT_BROKER_PROTO || 'mqtt';
+// const brokerUser = process.env.MQTT_BROKER_USER || 'user';
+// const brokerPasswd = process.env.MQTT_BROKER_PASSWD || 'passwd';
+// const trustedCaPath = process.env.MQTT_BROKER_CA || '';
+// const TRUSTED_CA = fs.readFileSync(trustedCaPath);
 
-var mqttClient = Mqtt.connect(
-    {
-        host: brokerUrl,
-        port: brokerPort,
-        protocol: mqttProtocol,
-        ca: TRUSTED_CA,
-        username: brokerUser,
-        password: brokerPasswd
-    }
-);
-
-mqttClient.on('error', (err) => {
-    console.error('MQTT Error:', err);
-});
+// var mqttClient = Mqtt.connect(
+//     {
+//         host: brokerUrl,
+//         port: brokerPort,
+//         protocol: mqttProtocol,
+//         ca: TRUSTED_CA,
+//         username: brokerUser,
+//         password: brokerPasswd
+//     }
+// );
+//
+// mqttClient.on('error', (err) => {
+//     console.error('MQTT Error:', err);
+// });
 
 var server = net.createServer((client) => {
     var gt06 = new Gt06();
@@ -58,8 +58,8 @@ var server = net.createServer((client) => {
             client.write(gt06.responseMsg);
         }
         gt06.msgBuffer.forEach(msg => {
-            mqttClient.publish(rootTopic + '/' + gt06.imei +
-                '/pos', JSON.stringify(msg));
+            // mqttClient.publish(rootTopic + '/' + gt06.imei +
+            //     '/pos', JSON.stringify(msg));
         });
         gt06.clearMsgBuffer();
     });
